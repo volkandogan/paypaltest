7e0613c07104e917af83d58cc1d5fb0fa78ee52c
volkan dogan <volkan.dogan@engineer.com>
2019-02-04 12:27:13 +0300

    paypal test

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Rest\ApiContext;
+use Illuminate\Http\Request;
+use PayPal\Api\Amount;
+use PayPal\Api\Details;
+use PayPal\Api\ExecutePayment;
+use PayPal\Api\Payment;
+use PayPal\Api\PaymentExecution;
+use PayPal\Api\Transaction;
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Api\Payer;
+use PayPal\Api\RedirectUrls;
+use Redirect;
+use Session;
+use URL;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Route;
+
+
+class PaymentController extends Controller
+{
+    private $_api_context;
+
+    public function __construct()
+    {
+        $payal_conf = \Config::get('paypal');
+        $this->_api_context = new ApiContext(new OAuthTokenCredential(
+            $payal_conf['client_id'],
+            $payal_conf['secret']
+        ));
+        $this->_api_context->setConfig($payal_conf['settings']);
+    }
+
+    public function payWithpaypal(Request $request)
+    {
+
+        $payer = new Payer();
+        $payer->setPaymentMethod('paypal');
+        $item_1 = new Item();
+        $item_1->setName('Item 1')
+        ->setCurrency('USD')
+            ->setQuantity(1)
+            ->setPrice($request->get('amount'));
+        /** unit price **/
+        $item_list = new ItemList();
+        $item_list->setItems(array($item_1));
+        $amount = new Amount();
+        $amount->setCurrency('USD')
+            ->setTotal($request->get('amount'));
+        $transaction = new Transaction();
+        $transaction->setAmount($amount)
+            ->setItemList($item_list)
+            ->setDescription('Your transaction description');
+        $redirect_urls = new RedirectUrls();
+        $redirect_urls->setReturnUrl(URL::to('status'))
+        ->setCancelUrl(URL::to('status'));
+        $payment = new Payment();
+        $payment->setIntent('Sale')
+            ->setPayer($payer)
+            ->setRedirectUrls($redirect_urls)
+            ->setTransactions(array($transaction));
+        try {
+            $payment->create($this->_api_context);
+        } catch (\PayPal\Exception\PPConnectionException $ex) {
+            if (\Config::get('app.debug')) {
+                \Session::put('error', 'Connection timeout');
+                return Redirect::to('/');
+            } else {
+                \Session::put('error', 'Some error occur, sorry for inconvenient');
+                return Redirect::to('/');
+            }
+        }
+        foreach ($payment->getLinks() as $link) {
+            if ($link->getRel() == 'approval_url') {
+                $redirect_url = $link->getHref();
+                break;
+            }
+        }
+        /** add payment ID to session **/
+        Session::put('paypal_payment_id', $payment->getId());
+        //dd($payment->getId());
+        if (isset($redirect_url)) {
+            /** redirect to paypal **/
+            return Redirect::away($redirect_url);
+        }
+        \Session::put('error', 'Unknown error occurred');
+        return Redirect::to('/');
+    }
+
+    public function getPaymentStatus()
+    {
+        /** Get the payment ID before session clear **/
+        $payment_id = Session::get('paypal_payment_id');
+        /** clear the session payment ID **/
+        //Session::forget('paypal_payment_id');
+        if (empty(Input::get('PayerID')) || empty(Input::get('token'))) {
+            \Session::put('error', 'Payment failed');
+            return Redirect::to('/');
+        }
+        $payment = Payment::get($payment_id, $this->_api_context);
+
+        $execution = new PaymentExecution();
+        $execution->setPayerId(Input::get('PayerID'));
+        /**Execute the payment **/
+        $result = $payment->execute($execution, $this->_api_context);
+        if ($result->getState() == 'approved') {
+            \Session::put('success', 'Payment success');
+
+
+            return Redirect::to('/');
+        }
+        \Session::put('error', 'Payment failed');
+        return Redirect::to('/');
+    }
+}
